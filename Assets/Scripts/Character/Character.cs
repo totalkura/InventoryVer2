ede7d5f17e645eef1cebba79d23f45b3a4c19b2c
SMC <tyuui88@gmail.com>
2025-08-27 21:21:12 +0900

    feat : 인벤토리 세팅 및 스크립트 생성

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,25 @@
+public class Character 
+{
+    public string userName { get; private set; }
+    public int userAtt {  get; private set; }
+    public int userDef { get; private set; }
+    public int userHP { get; private set; }
+    public int userCri { get; private set; }
+    public int userLevel { get; private set; }
+    public int userMaxExp { get; private set; }
+    public int userNowExp { get; private set; }
+    public int userEquip { get; private set; }
+
+    public Character (string username, int useratt, int userdef, int userhp, int usercri, int userlevel, int usermaxexp, int usernowexp)
+    {
+        userName = username;
+        userAtt = useratt;
+        userDef = userdef;
+        userHP = userhp;
+        userCri = usercri;
+        userLevel = userlevel;
+        userMaxExp = usermaxexp;
+        userNowExp = usernowexp;
+        userEquip = -1;
+    }
+}
